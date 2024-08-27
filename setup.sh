3cc8a0f681e7938bbc8a062b0d892a1a00843e1b
wadahana <wadahana@gmail.com>
2024-08-27 09:23:13 +0800

    misc : add setup

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ apt-get install -y git
 apt-get install -y ffmpeg
 
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple 
-pip install torch torchvision torchaudio --proxy=http://192.168.8.222:8081 --index-url https://download.pytorch.org/whl/cu118
-
+pip install torch torchvision torchaudio --proxy=http://192.168.124.100:8080 --index-url https://download.pytorch.org/whl/cu118
+pip install numpy==1.26.4
 ln -s /usr/local/lib/python3.10/dist-packages/nvidia/cuda_nvrtc/lib/libnvrtc.so.11.2 /usr/local/lib/python3.10/dist-packages/nvidia/cuda_nvrtc/lib/libnvrtc.so
 pip cache purge
 apt-get autoclean
